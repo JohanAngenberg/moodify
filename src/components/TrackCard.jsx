fdc99aee279fc756af7e0f6da21b8c49ec36621f
JohanAngenberg <johan.angenberg@gmail.com>
2019-04-01 14:55:47 +0200

    updated trackcard.jsx for live data

--- src/components/TrackCard.jsx
+++ src/components/TrackCard.jsx
@@ -4,35 +4,19 @@ export class TrackCard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            Trackdata: {
-                "danceability": 0.465,
-                "energy": 0.988,
-                "key": 9,
-                "loudness": -4.143,
-                "mode": 1,
-                "speechiness": 0.271,
-                "acousticness": 0.00194,
-                "instrumentalness": 0.11,
-                "liveness": 0.335,
-                "valence": 0.254,
-                "tempo": 140.15,
-                "type": "audio_features",
-                "id": "28lIyNNveMZ5UwvRoWMZlC",
-                "uri": "spotify:track:28lIyNNveMZ5UwvRoWMZlC",
-                "track_href": "https://api.spotify.com/v1/tracks/28lIyNNveMZ5UwvRoWMZlC",
-                "analysis_url": "https://api.spotify.com/v1/audio-analysis/28lIyNNveMZ5UwvRoWMZlC",
-                "duration_ms": 301939,
-                "time_signature": 4,
-                "artist": "The Winery Dogs",
-                "songName": "Elevate",
-                "image": "https://i.scdn.co/image/349c62416ca9f5d092c4daeed16f46c6723a5679"
-            },
-            isLoading: false
+            TrackData: {},
+            isLoading: true
         }
+    }
+    componentDidMount() {
+        this.setState({
+            Trackdata: this.props.track,
+            isLoading: false
+        })
     }
 
     render() {
-        const track = this.state.Trackdata;
+        const track = this.state.TrackData;
         const rowWrapperStyles = {
             fontSize: '10px',
             contentHeight: '60px',
