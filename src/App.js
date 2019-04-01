ad628d5e2e7a21a67a6c4fe500d606f1186fa66f
Emma Bernhardsson <emmafia001@gmail.com>
2019-04-01 14:42:15 +0200

    Added class text in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Tracklist from './components/Tracklist';
 
+class App extends Component {
   render() {
     return (
       <div>
@@ -9,6 +10,7 @@ import Tracklist from './components/Tracklist';
       </div>
     );
   }
+}
 
 
 export default App;
