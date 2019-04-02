5542d9fed24febe348197b1c00c05f8d3d99ad3c
Tobias Hedin <tobias.hedin@gmail.com>
2019-04-02 17:03:46 +0200

    nothing changed

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
-import { TrackCard } from './TrackCard';
-import { getUpperLimit, getLowerLimit } from '../utilities/filterHelpFunctions.js';
+import React, { Component } from "react";
+import { TrackCard } from "./TrackCard";
+import {
+  getUpperLimit,
+  getLowerLimit
+} from "../utilities/filterHelpFunctions.js";
 
-let dummyPlaylist = require('../dummydata.json');     //DETTA ÄR DUMMYPLAYLIST
+let dummyPlaylist = require("../dummydata.json"); //DETTA ÄR DUMMYPLAYLIST
 
 class TrackList extends Component {
   constructor(props) {
@@ -22,39 +25,64 @@ class TrackList extends Component {
         console.log(res);
         //this.setState({ originalPlaylist: res })
         //TA BORT // FRÅN OVANSTÅENDE FÖR ATT KOMMA ÅT BACKEND-DATA
-      })
+      });
   }
 
   updatePlaylist(e) {
-    this.setState({ playlist: e })
+    this.setState({ playlist: e });
   }
 
   render() {
     console.log(this.props.filterValues);
-    let { danceability, energy, acousticness, instrumentalness, valence, tempo } = this.props.filterValues;
+    let {
+      danceability,
+      energy,
+      acousticness,
+      instrumentalness,
+      valence,
+      tempo
+    } = this.props.filterValues;
     const playlist = this.state.originalPlaylist
-      .filter(track => danceability === '' ? track : (track.danceability >= getLowerLimit(danceability) && track.danceability <= getUpperLimit(danceability)))
-      .filter(track => energy === '' ? track : (track.energy >= getLowerLimit(energy) && track.energy <= getUpperLimit(energy)))
-      .filter(track => acousticness === '' ? track : (track.acousticness >= getLowerLimit(acousticness) && track.acousticness <= getUpperLimit(acousticness)))
-      .filter(track => instrumentalness === '' ? track : (track.instrumentalness >= getLowerLimit(instrumentalness) && track.instrumentalness <= getUpperLimit(instrumentalness)))
-      .filter(track => valence === '' ? track : (track.valence >= getLowerLimit(valence) && track.valence <= getUpperLimit(valence)))
-      .filter(track => tempo === '' ? track : (track.tempo >= getLowerLimit(tempo) && track.tempo <= getUpperLimit(tempo)))
-      .map((track, i) => (
-        <TrackCard key={i} track={track}></TrackCard>
-      ));
-<<<<<<< HEAD
+      .filter(track =>
+        danceability === ""
+          ? track
+          : track.danceability >= getLowerLimit(danceability) &&
+            track.danceability <= getUpperLimit(danceability)
+      )
+      .filter(track =>
+        energy === ""
+          ? track
+          : track.energy >= getLowerLimit(energy) &&
+            track.energy <= getUpperLimit(energy)
+      )
+      .filter(track =>
+        acousticness === ""
+          ? track
+          : track.acousticness >= getLowerLimit(acousticness) &&
+            track.acousticness <= getUpperLimit(acousticness)
+      )
+      .filter(track =>
+        instrumentalness === ""
+          ? track
+          : track.instrumentalness >= getLowerLimit(instrumentalness) &&
+            track.instrumentalness <= getUpperLimit(instrumentalness)
+      )
+      .filter(track =>
+        valence === ""
+          ? track
+          : track.valence >= getLowerLimit(valence) &&
+            track.valence <= getUpperLimit(valence)
+      )
+      .filter(track =>
+        tempo === ""
+          ? track
+          : track.tempo >= getLowerLimit(tempo) &&
+            track.tempo <= getUpperLimit(tempo)
+      )
+      .map((track, i) => <TrackCard key={i} track={track} />);
     console.log(playlist);
 
-=======
-      console.log(playlist)
-      
->>>>>>> c17f4f67a7e899777978ea62b6604a2dbe0b1044
-
-    return (
-      <div className="App">
-        {playlist}
-      </div>
-    );
+    return <div className="App">{playlist}</div>;
   }
 }
 
