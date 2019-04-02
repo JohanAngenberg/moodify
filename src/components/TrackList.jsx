afecdc05452bb95ba64f7c3d86b4d2edc2cd0a53
Emma Bernhardsson <emmafia001@gmail.com>
2019-04-02 15:02:43 +0200

    Added filter function in TrackList component

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -1,46 +1,48 @@
 import React, { Component } from 'react';
 import { TrackCard } from './TrackCard';
-
-
+import { getUpperLimit, getLowerLimit } from '../utilities/filterHelpFunctions.js';
 
 let dummyPlaylist = require('../dummydata.json');     //DETTA ÄR DUMMYPLAYLIST
 
-  
 class TrackList extends Component {
   constructor(props) {
-  super(props);
+    super(props);
 
-  this.state = {
-    originalPlaylist: dummyPlaylist
-    //TA BORT dummyPlaylist OCH ERSÄTT MED [] FÖR ATT HÄMTA BACKEND-DATA
-  };
-}
+    this.state = {
+      originalPlaylist: dummyPlaylist
+      //TA BORT dummyPlaylist OCH ERSÄTT MED [] FÖR ATT HÄMTA BACKEND-DATA
+    };
+  }
 
-componentDidMount() {
-  fetch('http://localhost:3001/audio-features/5cc2hF6vD6wDqq3sksQFCK')
-    .then(res => res.json())
-    .then(res => {
-      console.log(res);
-      //this.setState({ originalPlaylist: res })
-      //TA BORT // FRÅN OVANSTÅENDE FÖR ATT KOMMA ÅT BACKEND-DATA
-    })
+  componentDidMount() {
+    fetch('http://localhost:3001/audio-features/5cc2hF6vD6wDqq3sksQFCK')
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        //this.setState({ originalPlaylist: res })
+        //TA BORT // FRÅN OVANSTÅENDE FÖR ATT KOMMA ÅT BACKEND-DATA
+      })
+  }
 
-}
+  render() {
+    let { danceability, energy, acousticness, instrumentalness, valence, tempo } = this.props.filterValues;
+    const playlist = this.state.originalPlaylist
+      .filter(track => danceability === '' ? track : (track.danceability >= getLowerLimit(danceability) && track.danceability <= getUpperLimit(danceability)))
+      .filter(track => energy === '' ? track : (track.energy >= getLowerLimit(energy) && track.energy <= getUpperLimit(energy)))
+      .filter(track => acousticness === '' ? track : (track.acousticness >= getLowerLimit(acousticness) && track.acousticness <= getUpperLimit(acousticness)))
+      .filter(track => instrumentalness === '' ? track : (track.instrumentalness >= getLowerLimit(instrumentalness) && track.instrumentalness <= getUpperLimit(instrumentalness)))
+      .filter(track => valence === '' ? track : (track.valence >= getLowerLimit(valence) && track.valence <= getUpperLimit(valence)))
+      .filter(track => tempo === '' ? track : (track.tempo >= getLowerLimit(tempo) && track.tempo <= getUpperLimit(tempo)))
+      .map((track, i) => (
+        <TrackCard key={i} track={track}></TrackCard>
+      ));
 
-
-
-render() {
-  const playlist = this.state.originalPlaylist
-    .map((track, i) => (
-      <TrackCard key={i} track={track}></TrackCard>
-    ));
-
-  return (
-    <div className="App">
-      {playlist}
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        {playlist}
+      </div>
+    );
+  }
 }
 
 export default TrackList;
