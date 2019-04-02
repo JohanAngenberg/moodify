b271eaab58c34bdb3c5f0b213efc0f3b2ea85e47
Emma Bernhardsson <emmafia001@gmail.com>
2019-04-02 15:25:25 +0200

    Made changes in filter limit functions

--- src/utilities/filterHelpFunctions.js
+++ src/utilities/filterHelpFunctions.js
@@ -1,10 +1,8 @@
 
 export function getUpperLimit(value) {
-    let halfInterval = 0.1 //Corresponds to +/- 10%
-    return value * (1 + halfInterval);
+    return value + 0.1;
 }
 
 export function getLowerLimit(value) {
-    let halfInterval = 0.1 //Corresponds to +/- 10%
-    return value * (1 - halfInterval);
+    return value - 0.1;
 }
