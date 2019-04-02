d046c62b3abd1615cba05c5d8e97158fcd64dc04
JohanAngenberg <johan.angenberg@gmail.com>
2019-04-02 16:02:53 +0200

    updated tempo func

--- src/components/ToolKit.jsx
+++ src/components/ToolKit.jsx
@@ -34,15 +34,47 @@ class ToolKit extends React.Component {
         return (
             <div style={slideWrapperStyles}>
                 <div style={{ display: 'flex' }}>
-                    <div style={{ flex: '65%' }}>
-                        <InputRange step={1} minValue={0} maxValue={10} value={this.state.danceability * 10} onChange={value => { this.setState({ danceability: value / 10 }); this.props.danceability(this.state.danceability) }} />
-                    </div>
+                    <InputRange step={1} minValue={0} maxValue={10} value={this.state.danceability * 10}
+                        onChange={value => {
+                            this.setState({ danceability: value / 10 });
+                            this.props.danceability(this.state.danceability)
+                        }} />
                 </div>
-                <div><InputRange step={1} minValue={0} maxValue={10} value={this.state.energy * 10} onChange={value => { this.setState({ energy: value / 10 }); this.props.energy(this.state.energy) }} /></div>
-                <div><InputRange step={1} minValue={0} maxValue={10} value={this.state.acousticness * 10} onChange={value => { this.setState({ acousticness: value / 10 }); this.props.acousticness(this.state.acousticness) }} /></div>
-                <div><InputRange step={1} minValue={0} maxValue={10} value={this.state.instrumentalness * 10} onChange={value => { this.setState({ instrumentalness: value / 10 }); this.props.instrumentalness(this.state.instrumentalness) }} /></div>
-                <div><InputRange step={1} minValue={0} maxValue={10} value={this.state.valence * 10} onChange={value => { this.setState({ valence: value / 10 }); this.props.valence(this.state.valence) }} /></div>
-                <div><InputRange step={10} minValue={60} maxValue={300} value={this.state.tempo * 10} onChange={value => { this.setState({ tempo: value / 10 }); this.props.tempo(this.state.tempo) }} /></div>
+                <div>
+                    <InputRange step={1} minValue={0} maxValue={10} value={this.state.energy * 10}
+                        onChange={value => {
+                            this.setState({ energy: value / 10 });
+                            this.props.energy(this.state.energy)
+                        }} />
+                </div>
+                <div>
+                    <InputRange step={1} minValue={0} maxValue={10} value={this.state.acousticness * 10}
+                        onChange={value => {
+                            this.setState({ acousticness: value / 10 });
+                            this.props.acousticness(this.state.acousticness)
+                        }} />
+                </div>
+                <div>
+                    <InputRange step={1} minValue={0} maxValue={10} value={this.state.instrumentalness * 10}
+                        onChange={value => {
+                            this.setState({ instrumentalness: value / 10 });
+                            this.props.instrumentalness(this.state.instrumentalness)
+                        }} />
+                </div>
+                <div>
+                    <InputRange step={1} minValue={0} maxValue={10} value={this.state.valence * 10}
+                        onChange={value => {
+                            this.setState({ valence: value / 10 });
+                            this.props.valence(this.state.valence)
+                        }} />
+                </div>
+                <div>
+                    <InputRange step={10} minValue={60} maxValue={300} value={this.state.tempo}
+                        onChange={value => {
+                            this.setState({ tempo: value });
+                            this.props.tempo(this.state.tempo)
+                        }} />
+                </div>
             </div>
         )
     }
