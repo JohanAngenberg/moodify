a4509fd4bff19815c6c1ed25be25ab258e844acf
Emma Bernhardsson <emmafia001@gmail.com>
2019-04-03 17:28:18 +0200

    Fixed issue if wrong playlist id is entered

--- src/components/HeadSearch.jsx
+++ src/components/HeadSearch.jsx
@@ -22,17 +22,29 @@ class HeadSearch extends Component {
   handleKeyPress(e) {
     if (e.key === "Enter") {
       this.props.playlistUri(this.state.filterInput);
-      console.log(this.state.filterInput);
+
+      // console.log(this.state.filterInput);
 
       fetch(`http://localhost:3001/audio-features/${this.state.filterInput}`)
         .then(res => res.json())
         .then(res => {
           console.log(res);
+          if (res.length !== 0) {
+            console.log('Data finns');
+            return res;
+          } else {
+            console.log('Felhantering!');
+          }
+        })
+        .then(res => console.log(res))
+        .then(res => {
           this.setState({ playlist: res });
         })
         .then(this.props.playlist(this.state.playlist));
     }
   }
+
+
 
   handleInputTextChange(e) {
     let playlistId = getPlaylistId(e.target.value);
