4045449de8e1eea8fac066a3aed4765596ee6ce6
JohanAngenberg <johan.angenberg@gmail.com>
2019-04-03 13:59:07 +0200

    fixed filtering on toolkit

--- src/components/ToolKit.jsx
+++ src/components/ToolKit.jsx
@@ -8,27 +8,68 @@ class ToolKit extends React.Component {
         super(props);
 
         this.state = {
-            danceability: 0,
-            energy: 0,
-            acousticness: 0,
-            instrumentalness: 0,
-            valence: 0,
-            tempo: 0,
-            toggleDance: false
+            danceability: '',
+            energy: '',
+            acousticness: '',
+            instrumentalness: '',
+            valence: '',
+            tempo: '',
+            toggleDance: false,
+            toggleEnergy: false,
+            toggleAcousticness: false,
+            toggleInstrumentalness: false,
+            toggleValence: false,
+            toggleTempo: false,
         };
     }
 
     onToggleDance() {
-        this.setState({ toggleDance: !this.state.toggleDance });
         if (!this.state.toggleDance) {
-            this.setState({
-                danceability: ""
-            })
+            this.props.danceability(this.state.danceability)
         } else {
-            this.setState({
-                danceability: 0
-            })
+            this.props.danceability("")
         }
+        this.setState({ toggleDance: !this.state.toggleDance })
+    }
+    onToggleEnergy() {
+        if (!this.state.toggleEnergy) {
+            this.props.energy(this.state.energy)
+        } else {
+            this.props.energy("")
+        }
+        this.setState({ toggleEnergy: !this.state.toggleEnergy })
+    }
+    onToggleAcousticness() {
+        if (!this.state.toggleAcousticness) {
+            this.props.acousticness(this.state.acousticness)
+        } else {
+            this.props.acousticness("")
+        }
+        this.setState({ toggleAcousticness: !this.state.toggleAcousticness });
+    }
+    onToggleInstrumentalness() {
+        if (!this.state.toggleInstrumentalness) {
+            this.props.instrumentalness(this.state.instrumentalness)
+        } else {
+            this.props.instrumentalness("")
+        }
+        this.setState({ toggleInstrumentalness: !this.state.toggleInstrumentalness });
+    }
+    onToggleValence() {
+        if (!this.state.toggleValence) {
+            this.props.valence(this.state.valence)
+        } else {
+            this.props.valence("")
+        }
+        this.setState({ toggleValence: !this.state.toggleValence });
+    }
+    onToggleTempo() {
+        if (!this.state.toggleTempo) {
+            this.props.tempo(this.state.tempo)
+        } else {
+            this.props.tempo("")
+        }
+        this.setState({ toggleTempo: !this.state.toggleTempo });
     }
 
     render() {
@@ -37,7 +78,7 @@ class ToolKit extends React.Component {
             display: 'flex',
             width: '100%',
             flexDirection: 'column',
-            justifyContent: 'space-evenly',
+            justifyContent: 'space-between',
 
         }
         const barWrapperStyles = {
@@ -51,69 +92,104 @@ class ToolKit extends React.Component {
             marginBottom: '20px',
             fontSize: '12px',
         }
+
+        const toolsWrappertStyles = {
+            display: 'flex'
+        }
+        const buttonWrapperStyles = {
+            height: '100%',
+            display: 'flex',
+            width: '25%',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+        }
+        const buttonStyles = {
+            height: '80px'
+        }
         return (
-            <div style={slideWrapperStyles}>
-                <div style={barWrapperStyles}>
-                    <div style={barLabelStyles}>Danceability</div>
-                    <div style={{ flexDirection: 'row', width: '100%' }}>
-                        <div style={{}}>
+            <div>
+                <div style={toolsWrappertStyles}>
+                    <div style={buttonWrapperStyles}>
+                        <div style={{ height: '80px', marginTop: '30px' }}>
                             <Button onClick={this.onToggleDance.bind(this)} size="sm" variant={this.state.toggleDance ? 'info' : 'outline-info'}>{this.state.toggleDance ? 'ON' : 'OFF'}</Button>
                         </div>
-                        <div style={{}}>
-                            <InputRange disabled={!this.state.toggleDance} step={1} minValue={0} maxValue={10} value={this.state.danceability * 10}
+                        <div style={buttonStyles}>
+                            <Button onClick={this.onToggleEnergy.bind(this)} size="sm" variant={this.state.toggleEnergy ? 'info' : 'outline-info'}>{this.state.toggleEnergy ? 'ON' : 'OFF'}</Button>
+                        </div>
+                        <div style={buttonStyles}>
+                            <Button onClick={this.onToggleAcousticness.bind(this)} size="sm" variant={this.state.toggleAcousticness ? 'info' : 'outline-info'}>{this.state.toggleAcousticness ? 'ON' : 'OFF'}</Button>
+                        </div>
+                        <div style={buttonStyles}>
+                            <Button onClick={this.onToggleInstrumentalness.bind(this)} size="sm" variant={this.state.toggleInstrumentalness ? 'info' : 'outline-info'}>{this.state.toggleInstrumentalness ? 'ON' : 'OFF'}</Button>
+                        </div>
+                        <div style={buttonStyles}>
+                            <Button onClick={this.onToggleValence.bind(this)} size="sm" variant={this.state.toggleValence ? 'info' : 'outline-info'}>{this.state.toggleValence ? 'ON' : 'OFF'}</Button>
+                        </div>
+                        <div style={buttonStyles}>
+                            <Button onClick={this.onToggleTempo.bind(this)} size="sm" variant={this.state.toggleTempo ? 'info' : 'outline-info'}>{this.state.toggleTempo ? 'ON' : 'OFF'}</Button>
+                        </div>
+                    </div>
+
+                    <div style={slideWrapperStyles}>
+                        <div style={barWrapperStyles}>
+                            <div style={barLabelStyles}>Danceability</div>
+                            <div style={{ flexDirection: 'row', width: '100%' }}>
+
+                                <div>
+                                    <InputRange disabled={!this.state.toggleDance} step={1} minValue={0} maxValue={10} value={this.state.danceability * 10}
+                                        onChange={value => {
+                                            this.setState({ danceability: value / 10 });
+                                            this.props.danceability(value / 10)
+                                        }} />
+                                </div>
+                            </div>
+                        </div>
+                        <div style={barWrapperStyles}>
+                            <div style={barLabelStyles}>Energy</div>
+                            <InputRange disabled={!this.state.toggleEnergy} step={1} minValue={0} maxValue={10} value={this.state.energy * 10}
                                 onChange={value => {
-                                    this.setState({ danceability: value / 10 });
-                                    this.props.danceability(value / 10)
+                                    this.setState({ energy: value / 10 });
+                                    this.props.energy(value / 10)
+                                }} />
+                        </div>
+                        <div style={barWrapperStyles}>
+                            <div style={barLabelStyles}>Acousticness</div>
+                            <InputRange disabled={!this.state.toggleAcousticness} step={1} minValue={0} maxValue={10} value={this.state.acousticness * 10}
+                                onChange={value => {
+                                    this.setState({ acousticness: value / 10 });
+                                    this.props.acousticness(value / 10)
+                                }} />
+                        </div>
+                        <div style={barWrapperStyles}>
+                            <div style={barLabelStyles}>Instrumentalness</div>
+                            <InputRange disabled={!this.state.toggleInstrumentalness} step={1} minValue={0} maxValue={10} value={this.state.instrumentalness * 10}
+                                onChange={value => {
+                                    this.setState({ instrumentalness: value / 10 });
+                                    this.props.instrumentalness(value / 10)
+                                }} />
+                        </div>
+                        <div style={barWrapperStyles}>
+                            <div style={barLabelStyles}>Valence</div>
+                            <InputRange disabled={!this.state.toggleValence} step={1} minValue={0} maxValue={10} value={this.state.valence * 10}
+                                onChange={value => {
+                                    this.setState({ valence: value / 10 });
+                                    this.props.valence(value / 10)
+                                }} />
+                        </div>
+                        <div style={{
+                            paddingBottom: '25px',
+                            textAlign: 'center'
+                        }}>
+                            <div style={barLabelStyles}>Tempo</div>
+                            <InputRange disabled={!this.state.toggleTempo} step={10} minValue={60} maxValue={300} value={this.state.tempo}
+                                onChange={value => {
+                                    this.setState({ tempo: value });
+                                    this.props.tempo(value)
                                 }} />
                         </div>
                     </div>
                 </div>
-                <div style={barWrapperStyles}>
-                    <div style={barLabelStyles}>Energy</div>
-                    <InputRange step={1} minValue={0} maxValue={10} value={this.state.energy * 10}
-                        onChange={value => {
-                            this.setState({ energy: value / 10 });
-                            this.props.energy(value / 10)
-                        }} />
-                </div>
-                <div style={barWrapperStyles}>
-                    <div style={barLabelStyles}>Acousticness</div>
-                    <InputRange step={1} minValue={0} maxValue={10} value={this.state.acousticness * 10}
-                        onChange={value => {
-                            this.setState({ acousticness: value / 10 });
-                            this.props.acousticness(value / 10)
-                        }} />
-                </div>
-                <div style={barWrapperStyles}>
-                    <div style={barLabelStyles}>Instrumentalness</div>
-                    <InputRange step={1} minValue={0} maxValue={10} value={this.state.instrumentalness * 10}
-                        onChange={value => {
-                            this.setState({ instrumentalness: value / 10 });
-                            this.props.instrumentalness(value / 10)
-                        }} />
-                </div>
-                <div style={barWrapperStyles}>
-                    <div style={barLabelStyles}>Valence</div>
-                    <InputRange step={1} minValue={0} maxValue={10} value={this.state.valence * 10}
-                        onChange={value => {
-                            this.setState({ valence: value / 10 });
-                            this.props.valence(value / 10)
-                        }} />
-                </div>
-                <div style={{
-                    paddingBottom: '25px',
-                    textAlign: 'center'
-                }}>
-                    <div style={barLabelStyles}>Tempo</div>
-                    <InputRange step={10} minValue={60} maxValue={300} value={this.state.tempo}
-                        onChange={value => {
-                            this.setState({ tempo: value });
-                            this.props.tempo(value)
-                        }} />
-                </div>
-                <Button variant="secondary" size="lg" block>
-                    Create Playlist
-                </Button>
+                <Button variant='info' block>Create Playlist</Button>
             </div>
         )
     }
