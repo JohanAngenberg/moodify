86f9419aaccd30f850dd3742adcb1a8b56d2e8ee
JohanAngenberg <johan.angenberg@gmail.com>
2019-04-03 10:22:52 +0200

    updated tracklist and toolkit styling

--- src/components/ToolKit.jsx
+++ src/components/ToolKit.jsx
@@ -30,45 +30,60 @@ class ToolKit extends React.Component {
             justifyContent: 'space-evenly',
 
         }
+        const barWrapperStyles = {
+            borderBottom: '1px solid gray',
+            paddingBottom: '25px',
+            textAlign: 'center'
+        }
 
+        const barLabelStyles = {
+            marginBottom: '20px',
+            fontSize: '12px',
+        }
         return (
             <div style={slideWrapperStyles}>
-                <div style={{ display: 'flex' }}>
+                <div style={barWrapperStyles}>
+                    <div style={barLabelStyles}>Danceability</div>
                     <InputRange step={1} minValue={0} maxValue={10} value={this.state.danceability * 10}
                         onChange={value => {
                             this.setState({ danceability: value / 10 });
                             this.props.danceability(value / 10)
                         }} />
                 </div>
-                <div>
+                <div style={barWrapperStyles}>
+                    <div style={barLabelStyles}>Energy</div>
                     <InputRange step={1} minValue={0} maxValue={10} value={this.state.energy * 10}
                         onChange={value => {
                             this.setState({ energy: value / 10 });
                             this.props.energy(value / 10)
                         }} />
                 </div>
-                <div>
+                <div style={barWrapperStyles}>
+                    <div style={barLabelStyles}>Acousticness</div>
                     <InputRange step={1} minValue={0} maxValue={10} value={this.state.acousticness * 10}
                         onChange={value => {
                             this.setState({ acousticness: value / 10 });
                             this.props.acousticness(value / 10)
                         }} />
                 </div>
-                <div>
+                <div style={barWrapperStyles}>
+                    <div style={barLabelStyles}>Instrumentalness</div>
                     <InputRange step={1} minValue={0} maxValue={10} value={this.state.instrumentalness * 10}
                         onChange={value => {
                             this.setState({ instrumentalness: value / 10 });
                             this.props.instrumentalness(value / 10)
                         }} />
                 </div>
-                <div>
+                <div style={barWrapperStyles}>
+                    <div style={barLabelStyles}>Valence</div>
                     <InputRange step={1} minValue={0} maxValue={10} value={this.state.valence * 10}
                         onChange={value => {
                             this.setState({ valence: value / 10 });
                             this.props.valence(value / 10)
                         }} />
                 </div>
-                <div>
+                <div style={barWrapperStyles}>
+                    <div style={barLabelStyles}>Tempo</div>
                     <InputRange step={10} minValue={60} maxValue={300} value={this.state.tempo}
                         onChange={value => {
                             this.setState({ tempo: value });
