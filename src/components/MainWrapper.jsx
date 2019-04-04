f0d5821f9d894cb7fb288ca24e02f09ffc462c0c
Tobias Hedin <tobias.hedin@gmail.com>
2019-04-04 10:29:08 +0200

    fixedconflicts

--- src/components/MainWrapper.jsx
+++ src/components/MainWrapper.jsx
@@ -3,11 +3,8 @@ import TrackList from "./TrackList";
 import HeadSearch from "./HeadSearch";
 import WebPlayer from "./WebPlayer";
 import ToolKit from "./ToolKit";
-<<<<<<< HEAD
 import QuickButton from "./QuickButton";
-=======
 import Modal from './Modal';
->>>>>>> 8acf8a76609b0dc0bc588613ce11090646e9d26e
 
 class MainWrapper extends React.Component {
     constructor(props) {
@@ -75,45 +72,6 @@ class MainWrapper extends React.Component {
         });
     }
 
-<<<<<<< HEAD
-  render() {
-    return (
-      <div>
-        <div className="header-container">
-          <HeadSearch
-            playlistUri={this.updatePlaylistUri.bind(this)}
-            playlist={this.updatePlaylist.bind(this)}
-          />
-        </div>
-        <div className="main-container">
-          <div className="track-container">
-            <TrackList
-              filterValues={this.state}
-              playlist={this.state.playlist}
-            />
-          </div>
-          <div className="right-column">
-            <div className="toolkit-container">
-              <QuickButton
-                danceability={this.updateDanceability.bind(this)}
-                energy={this.updateEnergy.bind(this)}
-                acousticness={this.updateAcousticness.bind(this)}
-                instrumentalness={this.updateInstrumentalness.bind(this)}
-                valence={this.updateValence.bind(this)}
-                tempo={this.updateTempo.bind(this)}
-              />
-              <ToolKit
-                danceability={this.updateDanceability.bind(this)}
-                energy={this.updateEnergy.bind(this)}
-                acousticness={this.updateAcousticness.bind(this)}
-                instrumentalness={this.updateInstrumentalness.bind(this)}
-                valence={this.updateValence.bind(this)}
-                tempo={this.updateTempo.bind(this)}
-              />
-            </div>
-            <div className="player-container">
-              <WebPlayer playlistUri={this.state.playlistUri} />
-=======
     toggleModal(event) {
         this.setState({
             showModal: event
@@ -138,6 +96,14 @@ class MainWrapper extends React.Component {
                     </div>
                     <div className="right-column">
                         <div className="toolkit-container">
+                            <QuickButton
+                                danceability={this.updateDanceability.bind(this)}
+                                energy={this.updateEnergy.bind(this)}
+                                acousticness={this.updateAcousticness.bind(this)}
+                                instrumentalness={this.updateInstrumentalness.bind(this)}
+                                valence={this.updateValence.bind(this)}
+                                tempo={this.updateTempo.bind(this)}
+                            />
                             <ToolKit
                                 danceability={this.updateDanceability.bind(this)}
                                 energy={this.updateEnergy.bind(this)}
@@ -159,7 +125,6 @@ class MainWrapper extends React.Component {
                         </div>
                     </div>
                 </div>
->>>>>>> 8acf8a76609b0dc0bc588613ce11090646e9d26e
             </div>
         );
     }
