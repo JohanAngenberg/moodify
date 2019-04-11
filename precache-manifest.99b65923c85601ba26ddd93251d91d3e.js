a8575aa1f0bb495751ecd3e1e16de2b3d0d36b8c
JohanAngenberg <johan.angenberg@gmail.com>
2019-04-11 21:15:16 +0200

    Updates

---
+++ precache-manifest.99b65923c85601ba26ddd93251d91d3e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/moodify/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/moodify/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/moodify/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/moodify/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "c7b5b511ac01d17d8509",
+    "url": "/moodify/static/js/runtime~main.6be1317b.js"
+  },
+  {
+    "revision": "4bdcd5e29a1b357e49fe",
+    "url": "/moodify/static/js/main.9a334e4d.chunk.js"
+  },
+  {
+    "revision": "175e5856d01e40940379",
+    "url": "/moodify/static/js/2.160a813f.chunk.js"
+  },
+  {
+    "revision": "4bdcd5e29a1b357e49fe",
+    "url": "/moodify/static/css/main.d5d8d9aa.chunk.css"
+  },
+  {
+    "revision": "175e5856d01e40940379",
+    "url": "/moodify/static/css/2.f750135a.chunk.css"
+  },
+  {
+    "revision": "ca986f625cbc0bc4a2859ab5125f1526",
+    "url": "/moodify/index.html"
+  }
+];
