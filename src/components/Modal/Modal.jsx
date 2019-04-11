dbee0b88fb9eaf357348b5735431ab26849d2886
slainedinvain <christianberglonn@gmail.com>
2019-04-11 11:47:31 +0200

    changed modal description

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -31,19 +31,17 @@ class Modal extends React.Component {
 
                         <h4>How I work, you ask?</h4>
                         <p className='infoTextModal'>
-                            1. Copy your playlist link from Spotify<br />
-                            2. Paste it in the input field behind this message<br />
-                            3. Press "Enter" and watch your playlist appear in front of you<br />
-                            4. Use our toolkit to filter out the songs you want and listen right away!<br />
+                            1. You can either select an existing playlist from our dropdown menu OR enter a Spotify playlist link of your own choice.<br />
+                            2. When you have chosen your playlist two alternatives will appear; Presets and Advanced. You can either choose one of our already done Presets OR use our toolkit to filter out the songs you want.<br />
                         </p>
                         <h4>But what does it MEAN?</h4>
                         <p className='infoTextModal'>
-                            Dance describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity.<br /><br />
-                            Energy represents a preceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Makes sense right?<br /><br />
-                            Acoustic measures whether a track is acoustic. Higher values represents higher confidence of the track being acoustic.<br /><br />
-                            Instrumental predicts whether a track contains no vocals. "Ooh" and "aah" sounds are treated as instrumental in this context.<br /><br />
-                            Positivity describes the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).<br /><br />
-                            Tempo shows the overall estimated tempo of a track in beats per minute (BPM).<br /><br />
+                            Dance describes how suitable a track is for dancing.<br /><br />
+                            Energy represents a preceptual measure of intensity and activity.<br /><br />
+                            Acoustic measures whether a track is acoustic.<br /><br />
+                            Instrumental predicts whether a track contains no vocals.<br /><br />
+                            Positivity describes the musical positiveness conveyed by a track.<br /><br />
+                            Tempo shows the overall tempo of a track in beats per minute (BPM).<br /><br />
                         </p>
                     </div>
                     <div className='modalFooter'>
